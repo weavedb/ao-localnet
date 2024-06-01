cd297e3da7d2ee7fd407ea85fb9ece5367bbd832
Tomoya Nagasawa <ocrybit@gmail.com>
2024-06-01 08:09:12 +0400

    auto-grant tokens to all generated wallets

--- seed/seed-for-aos.sh
+++ seed/seed-for-aos.sh
@@ -2,29 +2,11 @@
 
 cd $(dirname $0)
 
-#########################################################################################
 # Mint/grant tokens
-
-# # Give the Scheduler Location Publisher 1 AR
-# curl -q http://localhost:4000/mint/W8_WUWr_xaR9T8JwKwd-b861aq3W2iD3eVeaV_jOn-U/1000000000000
-# echo
-# # Give the `aos` Module Publisher 1 AR
-# curl -q http://localhost:4000/mint/OVH7RgGAzjOxqUPaWjYf1MTrnznM1zpSOTMfC78DpKc/1000000000000
-# echo
-# # Give the bundler service 1 AR
-# curl -q http://localhost:4000/mint/a55QnqSWizpWEnkTcPIijNYwuzTmk-TXTbXTM-CgjDc/1000000000000
-# echo
-# # Give the `ao` units 1 AR
-# curl -q http://localhost:4000/mint/JYti9w4-nAFUUAo7fE0gh4xcv_YxhNad-3Yj7pZbxP0/1000000000000
-# echo
-
-echo "!!!"
-echo "!!!"
-echo "!!!"
-echo "!!! Edit this script to grant tokens to your wallet(s)."
-echo "!!!"
-echo "!!!"
-echo "!!!"
+cd  node ../wallets/printWalletAddresses.mjs | while read -r line; do
+	address=$(echo $line | awk '{print $1}')
+	curl -q http://localhost:4000/mint/$address/1000000000000
+done
 
 #########################################################################################
 # Publish the 'Scheduler-Location' record
@@ -36,7 +18,6 @@ echo "!!!"
 # Publish the `aos` Module
 
 ./publish-aos-module.mjs
-echo
 echo "copy this   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 echo
 ./mine.mjs
